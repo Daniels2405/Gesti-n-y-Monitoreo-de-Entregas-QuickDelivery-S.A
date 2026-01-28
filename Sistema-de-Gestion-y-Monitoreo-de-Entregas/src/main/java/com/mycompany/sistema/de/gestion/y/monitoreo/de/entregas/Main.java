dc0d50e80ff4d794cb397d8b54415952fa29bf0a
Daniels2405 <danielbarrientossalas@hotmail.com>
2026-01-27 23:13:03 -0600

    Primer commit, inicialización del proyecto

---
+++ Sistema-de-Gestion-y-Monitoreo-de-Entregas/src/main/java/com/mycompany/sistema/de/gestion/y/monitoreo/de/entregas/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema.de.gestion.y.monitoreo.de.entregas;
+
+/**
+ *
+ * @author daniel-2405
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
