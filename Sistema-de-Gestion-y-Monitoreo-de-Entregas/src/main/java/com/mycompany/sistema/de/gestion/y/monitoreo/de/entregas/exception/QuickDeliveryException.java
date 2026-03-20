4ee827ea3197370f3f8a85859a5d4f41e9410090
Daniels2405 <danielbarrientossalas@hotmail.com>
2026-03-19 22:44:49 -0600

    estructura base del proyecto lista para el equipo

---
+++ Sistema-de-Gestion-y-Monitoreo-de-Entregas/src/main/java/com/mycompany/sistema/de/gestion/y/monitoreo/de/entregas/exception/QuickDeliveryException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistema.de.gestion.y.monitoreo.de.entregas.exception;
+
+/**
+ *
+ * @author daniel-2405
+ */
+public class QuickDeliveryException extends Exception {
+    public QuickDeliveryException(String message) {
+        super(message);
+    }
+}
